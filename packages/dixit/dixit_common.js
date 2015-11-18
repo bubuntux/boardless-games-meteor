ff4082e292db5029a23b0f7606fd70b867927313
Julio Gutierrez <bubuntux@gmail.com>
2015-11-18 11:23:16 -0600

    playing with ecma6

--- packages/dixit/dixit_common.js
+++ packages/dixit/dixit_common.js
@@ -1,14 +1,35 @@
-const DixitDescription = {
-	name: 'Dixit',
-	minPlayers: 3,
-	maxPlayers: 18 // # TODO ?
-};
+"use strict";
+const DixitDescription = GameDescription('Dixit', 3, 18);
 
 // Constants
 const MAX_CARDS = 450;
 const CARDS_PER_PLAYER = 6;
 const CARDS = _.range(1, MAX_CARDS);
 
-class Dixit {
+class GameDescription {
+	constructor(name, minPlayers, maxPlayers) {
+		this._name = name;
+		this._minPlayers = minPlayers;
+		this._maxPlayers = maxPlayers;
+	}
+	get name() {return this._name;}
+	get minPlayers() {return this._minPlayers;}
+	get maxPlayers() {return this._maxPlayers;}
+}
+
+class Game {
+	constructor(description) {
+		this.description = description;
+	}
+}
+
+class Dixit extends Game {
+	constructor() {
+		super(GameDescription());
+	}
+}
+var gameDescription = GameDescription('asdf', 2, 2);
+
+class GameService {
 
 }
